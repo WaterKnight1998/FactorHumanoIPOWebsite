cf97b62e04f57e4be621f81a14170b96f1ccb967
WaterKnight1998 <dvdlacallecastillo@gmail.com>
2020-03-07 18:18:17 +0100

    Updated Cloud Tag.

--- js/after-anchor.js
+++ js/after-anchor.js
@@ -2,7 +2,14 @@ function opencard(){
     var currentUrl = document.URL;
     segments   = currentUrl.split('#');
     if(segments.length > 1){
-        this.document.getElementById(segments[1]).getElementsByTagName("button")[0].click();
+        var aux = segments[1].split("-");
+        if(aux.length == 1){
+            this.document.getElementById(aux[0]).getElementsByTagName("button")[0].click();
+        }else if(aux.length == 2){
+            this.document.getElementById(aux[0]).getElementsByTagName("button")[0].click();
+            this.document.getElementById(aux[1]).getElementsByTagName("button")[0].click();
+            this.document.getElementById(aux[1]).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+        }  
     }
 }
 
